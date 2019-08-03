325985d297e37124d71ffbb8bd4f59e452abec2f
elradix <elrachid@gmail.com>
2019-08-03 21:51:14 +0200

    Easy+ Python v2.1

--- python_scripts/easyplus.py
+++ python_scripts/easyplus.py
@@ -1,31 +1,19 @@
-def doWork(hass, data):
-  sn = data.get('script_id')
-  ep = hass.states.get('switch.easyplus')
+sn = data.get('script_id')
+ep = hass.states.get('switch.easyplus')
 
-  if sn is None:
-    logger.warning('<easyplus> No script id was supplied.')
-    return
-
-  if ep is None:
-    logger.warning('<easyplus> switch.easyplus does not exist.')
-    return
-
+if sn is not None:
   ss = hass.states.get('script.' + sn)
+  if ss is not None:
+    if ep is not None:
+      if ep.state == 'off':
+        service_data = {'entity_id':'switch.easyplus'}
+        hass.services.call('switch', 'turn_on', service_data, False)    
+        time.sleep(17)
 
-  if ss is None:
+      hass.services.call('script', sn, '', False)
+    else:
+      logger.warning('<easyplus> switch.easyplus does not exist.')
+  else:
     logger.warning('<easyplus> Supplied script does not exist.')
-    return
-
-  if ep.state == 'off':
-    service_data = {'entity_id':'switch.easyplus'}
-    hass.services.call('switch', 'turn_on', service_data, False)
-    ep = hass.states.get('switch.easyplus')
-
-  wait_until = ep.last_changed + datetime.timedelta(seconds=17)
-  while wait_until > datetime.now():
-      time.sleep(1)
-
-  hass.services.call('script', sn, '', False)
-
-
-doWork(hass, data)
+else:
+  logger.warning('<easyplus> No script name was supplied.')

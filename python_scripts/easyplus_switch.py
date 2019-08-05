565e68516ab1100c50b9951961577a57bb1ce934
elradix <elrachid@gmail.com>
2019-08-06 00:49:20 +0200

    Script Magic v4

--- python_scripts/easyplus_switch.py
+++ python_scripts/easyplus_switch.py
@@ -1,19 +1,19 @@
-sn = data.get('switch_id')
+sn = data.get('entity_name')
 ep = hass.states.get('switch.easyplus')
 
 if sn is not None:
-  ss = hass.states.get('switch.' + sn)
+  ss = hass.states.get('entity_id.' + sn)
   if ss is not None:
     if ep is not None:
       if ep.state == 'off':
         service_data = {'entity_id':'switch.easyplus'}
         hass.services.call('switch', 'turn_on', service_data, False)
-        time.sleep(20)
+        time.sleep(17)
 
-        hass.services.call('switch', 'turn_on', service_data={ 'entity_id': 'sn' })
+      hass.services.call('switch', 'turn_on', service_data={ 'entity_id': 'sn' })
     else:
-      logger.warning('<easyplus> switch.easyplus does not exist.')
+      logger.warning('<easyplus> switch.easyplus does not exist')
   else:
-    logger.warning('<easyplus> Supplied script does not exist.')
+    logger.warning('<easyplus> switch does not exist')
 else:
-  logger.warning('<easyplus> No script name was supplied.')
+  logger.warning('<easyplus> no switch supplied')

84b273fd3caaca2bb17df46233f145e5508ccba0
elradix <elrachid@gmail.com>
2019-08-18 16:25:18 +0200

    update notification

--- python_scripts/easyplus_off.py
+++ python_scripts/easyplus_off.py
@@ -18,19 +18,29 @@ def doWork(hass, data, logger):
         service_data = {'entity_id':'script.lights'}
         hass.services.call('script', 'turn_on', service_data, False)
         time.sleep(15)
-        hass.services.call('notify', 'dageraad', {'message': 'Lights off'})
+        state = hass.states.get(ls).state
+        lights = (hass.states.get(ls).attributes["friendly_name"])
+        time.sleep(1)
+        hass.services.call('notify', 'dageraad', {'message': lights + ' are now ' + state })
         logger.info('Easyplus lights off')
 
   if sw.state == 'on':
         service_data = {'entity_id':'script.switches'}
         hass.services.call('script', 'turn_on', service_data, False)
         time.sleep(15)
-        hass.services.call('notify', 'dageraad', {'message': 'Switches off'})
-        logger.info('Easyplus switches off')
+        state = hass.states.get(sw).state
+        switches = (hass.states.get(sw).attributes["friendly_name"])
+        time.sleep(1)
+        hass.services.call('notify', 'dageraad', {'message': switches + ' are now ' + state })
+        logger.info('Easyplus Switches off')
 
   service_data = {'entity_id':'switch.easyplus'}
   hass.services.call('switch', 'turn_off', service_data, False)
-  hass.services.call('notify', 'dageraad', {'message': 'Easyplus off'})
-  logger.info('Easyplus off')
+  time.sleep(5)
+  state = hass.states.get(ep).state
+  switch = (hass.states.get(ep).attributes["friendly_name"])
+  time.sleep(1)
+  hass.services.call('notify', 'dageraad', {'message': switch + ' is now ' + state })
+  logger.info('Easyplus Switches off')
 
 doWork(hass, data, logger)

a6450b0f5e52b44105bb443ea5208ec7fce9a352
elradix <elrachid@gmail.com>
2019-08-18 13:54:22 +0200

    easyplus_switch v16

--- python_scripts/easyplus_switch.py
+++ python_scripts/easyplus_switch.py
@@ -29,7 +29,7 @@ def doWork(hass, data, logger):
                     {'message':'Switch status is {}'.format(ss)})
 
   hass.services.call('notify', 'dageraad',
-                    {'message': "The Switch" state })
+                    {'message': 'The Switch' state })
 
 #  hass.services.call('notify', 'dageraad', {'message': name  })
 

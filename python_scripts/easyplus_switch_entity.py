3dd39c018b66f39ec07587644a425935e766bb39
elradix <elrachid@gmail.com>
2019-09-08 23:25:10 +0200

    package heating

---
+++ python_scripts/easyplus_switch_entity.py
@@ -0,0 +1,30 @@
+def doWork(hass, data, logger):
+  sn = data.get('entity_id')
+  ep = hass.states.get('switch.easyplus')
+  ss = hass.states.get(sn)
+  switch = (hass.states.get(sn).attributes["friendly_name"])
+
+  if sn is None:
+    logger.warning('<easyplus_switch> no entity_id supplied')
+    return
+
+  if ep is None:
+    logger.warning('<easyplus_switch> switch.easyplus does not exist')
+    return
+
+  if ss is None:
+    logger.warning('<easyplus_switch> switch does not exist')
+    return
+
+  if ep.state == 'off':
+    service_data = {'entity_id':'switch.easyplus'}
+    hass.services.call('switch', 'turn_on', service_data, False)
+    time.sleep(20)
+
+  hass.services.call('homeassistant', 'toggle', service_data={ 'entity_id': sn })
+  time.sleep(5)
+  ss = hass.states.get(sn)
+  time.sleep(7)
+  hass.services.call('notify', 'dageraad', {'message': switch + ': ' + ss.state})
+
+doWork(hass, data, logger)

6c50070b16565b5b4379bbf9ed2a280dbf86786b
elradix <elrachid@gmail.com>
2019-11-17 18:33:56 +0100

    climate appdaemon v3

--- appdaemon/apps/climate.py
+++ appdaemon/apps/climate.py
@@ -4,7 +4,6 @@ class climate(hass.Hass):
  def initialize(self):
    for climate in self.args["entities"]:
      self.listen_state(self.climate_cb, climate, attribute='state')
-     self.log(self.args)
  def climate_cb(self, entity, attribute, old, new, kwargs):
    if old == "off" and new == "heat":
      self.log(climate_cb)

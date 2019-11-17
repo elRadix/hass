def32d584bb30feb8f7e6029635d81365bad3b74
elradix <elrachid@gmail.com>
2019-11-17 22:59:01 +0100

    climate

--- appdaemon/apps/climate.py
+++ appdaemon/apps/climate.py
@@ -1,37 +1,25 @@
 import appdaemon.plugins.hass.hassapi as hass
+import datetime
 import time
 class climate(hass.Hass):
 
  def initialize(self):
-   for climate in self.args["entities"]:
-     self.listen_state(self.climate_cb, climate, attribute='state')
-     self.log(self.args)
+   self.listen_state(self.climate_cb, self.args["climate"])
+
  def climate_cb(self, entity, attribute, old, new, kwargs):
-   self.log( "old state:", entity, old, new)
+   friendly = self.get_state(entity, attribute="friendly_name")
    if old == "off" and new == "heat":
-    self.log("new state:", entity, old, new)
-    if self.get_state('switch.easyplus', attribute='state') != 'on':
+    if self.get_state('switch.easyplus') != 'on':
       self.turn_on('switch.easyplus')
+      time.sleep(30)
       self.log("easyplus on")
-      time.sleep(20)
-    if self.get_state('input_boolean.easyplus_boiler_heating', attribute='state') != 'on':
+    if self.get_state('input_boolean.easyplus_boiler_heating') != 'on':
       self.turn_on('input_boolean.easyplus_boiler_heating')
       self.log("boiler on")
-    self.call_service("shell_command/heating_living")
-    self.call_service("shell_command/heating_eetkamer")
-    self.call_service("shell_command/heating_floradix")
-    self.call_service("shell_command/heating_sara")
-    self.call_service("shell_command/heating_yassin")
-    self.log("ON setpoint sent")
+    self.call_service("shell_command/heating_"+friendly)
+    self.log("target temperature set")
     return
    if old == "heat" and new == "off":
-    self.call_service("shell_command/heating_tmp_living_off")
-    # self.call_service('climate/set_temperature', entity_id=climate.living, target_temperature=5)
-    self.call_service("shell_command/heating_tmp_eetkamer_off")
-    self.call_service("shell_command/heating_tmp_slp1_off")
-    # self.call_service('climate/set_temperature', entity_id=climate.floradix, target_temperature=5)
-    self.call_service("shell_command/heating_tmp_slp2_off")
-    self.call_service("shell_command/heating_tmp_slp3_off")
-    self.call_service("shell_command/heating_tmp_eetkamer_off")
-    self.log("OFF setpoint sent")
+    self.call_service("shell_command/tmp_"+friendly+"_off")
+    self.log("target temperature off")
     return

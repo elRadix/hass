59373bda7aea998bf83143846f390f45bd85387f
elradix <elrachid@gmail.com>
2019-11-19 12:22:54 +0100

    shedy update v1

--- appdaemon/apps/climate.py
+++ appdaemon/apps/climate.py
@@ -4,15 +4,15 @@ import time
 class climate(hass.Hass):
 
  def initialize(self):
-   self.listen_state(self.climate_cb, self.args)
+   self.listen_state(self.climate_cb, self.args["climate"])
 
  def climate_cb(self, entity, attribute, old, new, kwargs):
    friendly = self.get_state(entity, attribute="friendly_name")
    if old == "off" and new == "heat":
     if self.get_state('switch.easyplus') != 'on':
       self.turn_on('switch.easyplus')
+      time.sleep(20)
       self.log("easyplus on")
-    time.sleep(30)
     if self.get_state('input_boolean.easyplus_boiler_heating_dev') != 'on':
       self.turn_on('input_boolean.easyplus_boiler_heating_dev')
       self.log("boiler on")
@@ -20,8 +20,8 @@ class climate(hass.Hass):
     self.log("target temperature set")
     return
    if old == "heat" and new == "off":
-    # self.call_service("climate/set_temperature", entity_id = self.args["climate"], temperature = 5)
+    self.call_service("climate/set_temperature", entity_id = self.args["climate"], temperature = 5)
     self.call_service("shell_command/heating_tmp_"+friendly+"_off")
     self.log("target temperature off")
     return
-
+   self.log(self.args)

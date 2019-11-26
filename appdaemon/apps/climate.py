826cad4aec858c366c99687fa5ba78e08351d701
elradix <elrachid@gmail.com>
2019-11-26 15:41:51 +0100

    update by Nordine v1

--- appdaemon/apps/climate.py
+++ appdaemon/apps/climate.py
@@ -16,13 +16,14 @@ class climate(hass.Hass):
    if old == "off" and new == "heat":
     for i in range (7):
       if telnet != 'on':
-        # self.get_state('binary_sensor.easyplus_telnet')
+        self.get_state('binary_sensor.easyplus_telnet')
         self.turn_off('switch.easyplus')
         self.turn_on('switch.easyplus')
         time.sleep(25)
         self.log("Failure - Telnet %s", telnet)
         tg = "Failure - Easyplus is {}, Telnet state is {} ".format(easyplus, telnet)
         self.call_service("notify/dageraad",message = tg)
+        self.get_state('binary_sensor.easyplus_telnet')
       else:
           break
     self.log("Succes - Telnet %s", telnet)

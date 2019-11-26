6c3548c368a1940b576bced59d6181ff2c7b6f94
elradix <elrachid@gmail.com>
2019-11-26 12:43:01 +0100

    notify 1

--- appdaemon/apps/notify.py
+++ appdaemon/apps/notify.py
@@ -6,18 +6,18 @@ class notify(appapi.AppDaemon):
     self.set_state("sensor.notify_message",state=" ")
     self.listen_state(self.send_notify,"sensor.notify_message")
 
+  def send_notify(self, entity, attribute, old, new, kwargs):
+    self.log("Updated Sensor reading - Value : " + new)
+    self.call_service('notify/dageraad',message = new)
+
+
 #   def send_notify(self, entity, attribute, old, new, kwargs):
-#     self.log("Updated Sensor reading - Value : " + new)
+#     self.log("Updated Sensor value : " + new)
 #     self.call_service('notify/dageraad', message=new)
 
+#     ha_trigger = self.get_state("sensor.notify_message","frontend")
+#     self.log("Frontend value : {}".format(ha_trigger))
 
-  def send_notify(self, entity, attribute, old, new, kwargs):
-    self.log("Updated Sensor value : " + new)
-    self.call_service('notify/dageraad', message=new)
-
-    ha_trigger = self.get_state("sensor.notify_message","frontend")
-    self.log("Frontend value : {}".format(ha_trigger))
-
-    if ha_trigger:
-        self.call_service('persistent_notification/create',
-            title="Attention", message=new+ " - " + datetime.now().strftime('@ %-I:%M %p') )
+#     if ha_trigger:
+#         self.call_service('persistent_notification/create',
+#             title="Attention", message=new+ " - " + datetime.now().strftime('@ %-I:%M %p') )

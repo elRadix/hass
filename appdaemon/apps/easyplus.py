a098606dc3ddf15b9da2088c048e07fff2215fab
elradix <elrachid@gmail.com>
2019-12-25 16:55:08 +0100

    Easyplus control by Merwone v8

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -21,7 +21,7 @@ class easyplus(hass.Hass):
          break
     cmd = '"' + error + '", shell=True'
     self.log("{}".format(cmd))
-    returncode = subprocess.run({cmd})
+    returncode = subprocess.run(cmd)
     self.log(self.args)
     telnet= 'Setrelay 32,1' ## make variable
     cmd = ["expect -f /opt/scripts/apex.sh ","{telnet}"]

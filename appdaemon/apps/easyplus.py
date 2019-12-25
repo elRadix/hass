8e48c9173183126c59208d1871e626129720ce57
elradix <elrachid@gmail.com>
2019-12-25 13:08:04 +0100

    Easyplus control v7

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -2,6 +2,8 @@ import appdaemon.plugins.hass.hassapi as hass
 import subprocess
 import datetime
 import time
+from subprocess import Popen, PIPE
+
 class easyplus(hass.Hass):
 
  def initialize(self):
@@ -9,7 +11,7 @@ class easyplus(hass.Hass):
 
  def error_cb(self, entity, attribute, old, new, kwargs):
     easyplus = self.get_state('binary_sensor.easyplus_telnet')
-    error = "['/usr/bin/expect', '-f', '"+str(new).split("-f ",1),[1]+"']"
+    error = "['/usr/bin/expect', '-f', '"+str(new).split("-f ",1)[1],+"']"
     self.log("{}".format(error))
     for i in range (0, 3, 1):
      if easyplus != 'on':
@@ -20,9 +22,11 @@ class easyplus(hass.Hass):
          self.log("easyplus turned {} for switch to work".format(easyplus))
          self.call_service("notify/dageraad", message = ("easyplus turned {} for switch to work".format(easyplus)))
          break
+    p1 = Popen(["dmesg"], stdout=PIPE)
+    print p1.communicate()
     cmd = str(error)
-    subprocess.call([cmd])
-    subprocess.call(["ls", "-l"])
-    self.log("{}".format(cmd))
-    self.log("{}".format(error))
+    #subprocess.call([cmd])
+    #subprocess.call(["ls", "-l"])
+    #self.log("{}".format(cmd))
+    #self.log("{}".format(error))
     self.log(self.args)

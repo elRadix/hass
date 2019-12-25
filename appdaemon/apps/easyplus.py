f7bedacfe9fffa6ec184c5852cf49746b83c5ea0
elradix <elrachid@gmail.com>
2019-12-25 16:11:40 +0100

    Easyplus control by Merwone v1

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -2,8 +2,6 @@ import appdaemon.plugins.hass.hassapi as hass
 import subprocess
 import datetime
 import time
-
-
 class easyplus(hass.Hass):
 
  def initialize(self):
@@ -11,28 +9,17 @@ class easyplus(hass.Hass):
 
  def error_cb(self, entity, attribute, old, new, kwargs):
     easyplus = self.get_state('binary_sensor.easyplus_telnet')
-#    error = "['/usr/bin/expect' '-f' '/opt/scripts/apex.sh' " + str(new).split(".sh ",1)[1] + "]"
-    error = "['/usr/bin/expect', '-f', '/opt/scripts/apex.sh', " + str(new).split(".sh ",1)[1] + "]"
-
+    error = "expect -f /opt/scripts/apex.sh " + str(new).split(".sh ",1)[1]
     self.log("{}".format(error))
     for i in range (0, 3, 1):
      if easyplus != 'on':
-#         self.turn_off('switch.easyplus')
- #        self.turn_on('switch.easyplus')
+         self.turn_on('switch.easyplus')
          time.sleep(20)
          easyplus = self.get_state('switch.easyplus')
          self.log("easyplus turned {} for switch to work".format(easyplus))
          self.call_service("notify/dageraad", message = ("easyplus turned {} for switch to work".format(easyplus)))
          break
-    run_cmd = str(error)
-    return_code =  subprocess.call([error])
-
-
-#    self.log("{}".format(cmd))
-    self.log("{}".format(error))
+    cmd = '"' + error + '", shell=True'
+    self.log("{}".format(cmd))
+    returncode = subprocess.run("expect -f /opt/scripts/apex.sh 'Setrelay 34,1'", shell=True)
     self.log(self.args)
-    # output = subprocess.getoutput(cmd)
-    # print(output)
-
-    # p = subprocess.Popen("expect -f /opt/scripts/apex.sh 'Setrelay 37,1", shell=True, stdout=subprocess.PIPE, stderr=subprocess.PIPE).communicate()[0]
-    # print p.split("\n")

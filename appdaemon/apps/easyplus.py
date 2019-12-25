ff2b9a6baf89615fbc646b2861111bad9912b5a7
elradix <elrachid@gmail.com>
2019-12-25 12:42:31 +0100

    Easyplus control v4

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -9,8 +9,8 @@ class easyplus(hass.Hass):
 
  def error_cb(self, entity, attribute, old, new, kwargs):
     easyplus = self.get_state('binary_sensor.easyplus_telnet')
-#    error = "['/usr/bin/expect', '-f', '"+str(new).split("-f ",1)[1]+",shell=True']"
-    error = "[/usr/bin/expect -f "+str(new).split("-f ",1)[1]+",shell=True]"
+    error = "['/usr/bin/expect', '-f', '"+str(new).split("-f ",1)[1]+",shell=True']"
+#    error = "[/usr/bin/expect -f "+str(new).split("-f ",1)[1]+",shell=True]"
 
     self.log("{}".format(error))
     for i in range (0, 3, 1):
@@ -23,6 +23,7 @@ class easyplus(hass.Hass):
          self.call_service("notify/dageraad", message = ("easyplus turned {} for switch to work".format(easyplus)))
          break
     cmd = str(error)
-    returncode = subprocess.call(cmd)
+    subprocess.call([cmd])
+    subprocess.call(["ls", "-l"])
     self.log("{}".format(error))
     self.log(self.args)

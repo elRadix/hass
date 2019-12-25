bacd3b2be16c2b502f9ae2ced2d9160a39904af6
elRadix <elrachid@gmail.com>
2019-12-25 11:49:58 +0100

    HACS

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -7,16 +7,20 @@ class easyplus(hass.Hass):
     self.listen_state(self.error_cb, 'sensor.error')
 
  def error_cb(self, entity, attribute, old, new, kwargs):
-    error = self.get_state(entity)
-    mylist = str(error).split("Command failed:",1)[1]
-    self.log("list is split, now printing the split list")
-    self.log(mylist)
-#    self.log("%s", mylist)
-    # if error.startswith("Command failed: "):
-    #    return error[17:]
-    # self.log("%s", error)
-    # self.log(self.args)
+    easyplus = self.get_state('binary_sensor.easyplus_telnet')
+    for i in range (0, 3, 1):
+     if easyplus != 'on':
+#         self.turn_off('switch.easyplus')
+         self.turn_on('switch.easyplus')
+         time.sleep(20)
+         easyplus = self.get_state('switch.easyplus')
+         self.log("easyplus turned {} for switch to work".format(easyplus))
+           break
+    error = str(new).split(":",1)[1]
+    cmd = ['error']
+    returncode = subprocess.call(cmd)
+    self.log("{}".format(error))
+    self.log(self.args)
 
 
 
-#    easyplus = self.get_state('binary_sensor.easyplus_telnet')

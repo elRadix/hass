79136b2051b0655967549fae770d3fde17957e0d
elradix <elrachid@gmail.com>
2019-12-27 09:44:20 +0100

    powerplugs update

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -1,28 +1,28 @@
-import appdaemon.plugins.hass.hassapi as hass
-import subprocess
-import datetime
-import time
-class easyplus(hass.Hass):
+# import appdaemon.plugins.hass.hassapi as hass
+# import subprocess
+# import datetime
+# import time
+# class easyplus(hass.Hass):
 
- def initialize(self):
-    self.listen_state(self.error_cb, 'sensor.error')
+#  def initialize(self):
+#     self.listen_state(self.error_cb, 'sensor.error')
 
- def error_cb(self, entity, attribute, old, new, kwargs):
-    easyplus = self.get_state('binary_sensor.easyplus_telnet')
-    error = "expect -f /opt/scripts/apex.sh " + str(new).split(".sh ",1)[1]
-    self.log("{}".format(error))
-    for i in range (0, 3, 1):
-     if easyplus != 'on':
-        # self.turn_on('switch.easyplus')
-        # time.sleep(20)
-         easyplus = self.get_state('switch.easyplus')
-         self.log("easyplus turned {} for switch to work".format(easyplus))
-         self.call_service("notify/dageraad", message = ("easyplus turned {} for switch to work".format(easyplus)))
-         break
-    cmd = '"' + error + '", shell=True'
-    telnet= 'Setrelay 32,1' ## make variable
-    cmds = ["expect -f /opt/scripts/apex.sh", "'{telnet}'"]
-    self.log("{}".format(cmds))
-    returncode = subprocess.run(cmds)
-    self.log(self.args)
-    subprocess.Popen(cmds)
+#  def error_cb(self, entity, attribute, old, new, kwargs):
+#     easyplus = self.get_state('binary_sensor.easyplus_telnet')
+#     error = "expect -f /opt/scripts/apex.sh " + str(new).split(".sh ",1)[1]
+#     self.log("{}".format(error))
+#     for i in range (0, 3, 1):
+#      if easyplus != 'on':
+#         # self.turn_on('switch.easyplus')
+#         # time.sleep(20)
+#          easyplus = self.get_state('switch.easyplus')
+#          self.log("easyplus turned {} for switch to work".format(easyplus))
+#          self.call_service("notify/dageraad", message = ("easyplus turned {} for switch to work".format(easyplus)))
+#          break
+#     cmd = '"' + error + '", shell=True'
+#     telnet= 'Setrelay 32,1' ## make variable
+#     cmds = ["expect -f /opt/scripts/apex.sh", '{telnet}']
+#     self.log("{}".format(cmds))
+#     returncode = subprocess.run(cmds)
+#     self.log(self.args)
+#     subprocess.Popen(cmds)

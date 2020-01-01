27aad41e3e8a324f37adf76b9eb324508b0aaf73
elradix <elrachid@gmail.com>
2020-01-01 14:20:34 +0100

    appdaemon updates v3

--- appdaemon/apps/easyplus.py
+++ appdaemon/apps/easyplus.py
@@ -15,7 +15,7 @@ class easyplus(hass.Hass):
     loop = 0
     self.log("{}".format(failed))
     if ",0" not in failed:
-      while self.get_state('binary_sensor.easyplus_telnet') == 'off' and loop != 4:
+      while self.get_state('binary_sensor.easyplus_telnet') == 'off' and loop <=4:
          loop+=1
          self.turn_off('switch.easyplus')
          time.sleep(2)

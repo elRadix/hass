fea2ec06cb9feebc8e1453c1f0695da873ae5169
elradix <elrachid@gmail.com>
2020-01-03 13:45:09 +0100

    AD Telnet v1.9

--- appdaemon/apps/telnet.py
+++ appdaemon/apps/telnet.py
@@ -8,14 +8,9 @@ class telnet(hass.Hass):
     self.listen_state(self.telnet_cb, 'input_boolean.night')
 
  def telnet_cb(self, entity, attribute, old, new, kwargs):
-    # script = "/opt/scripts/telnet.sh"
-    # result = subprocess.run(['/bin/sh', '/opt/scripts/telnet.sh'], shell=True, capture_output=True)
-    # print(result.stdout)
-    # print(result.stderr)
-    #returncode = subprocess.run("{} {}".format(script, failed), shell=True, capture_output=True).stdout
-    telnet = subprocess.run(['/bin/sh', '/opt/scripts/telnet.sh'], shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
+    telnet = subprocess.run(['/bin/sh', '/opt/scripts/telnet.sh'], shell=True, capture_output=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
     print(telnet.stdout)
-
+    self.log("{}".format(telnet))
     self.log(self.args)
 
 

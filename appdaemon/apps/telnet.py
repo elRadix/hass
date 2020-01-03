5847b85ff50d43dc98d741243f57874f49fc6326
elradix <elrachid@gmail.com>
2020-01-03 20:03:16 +0100

    AD Telnet v5.4

--- appdaemon/apps/telnet.py
+++ appdaemon/apps/telnet.py
@@ -10,21 +10,21 @@ class telnet(hass.Hass):
 
  def telnet_cb(self, entity, attribute, old, new, kwargs):
    os.chdir('/opt/scripts/') #to get into the directory where app is installed
-   process = subprocess.Popen(['python', '/opt/scripts/telnet.py'],
-                           stdout=subprocess.PIPE,
-                           universal_newlines=True)
-   while True:
-      output = process.stdout.readline()
-      print(output.strip())
-      # Do something else
-      return_code = process.poll()
-      if return_code is not None:
-         print('OUTPUT =', return_code)
-         # Process has finished, read rest of the output
-         for output in process.stdout.readlines():
-               print(output.strip())
-         break
+   telnet = subprocess.call(['python /opt/scripts/telnet.py'])
+   self.log(telnet)
 
-
-
-#   telnet = subprocess.call(['/opt/scripts/telnet.py'])
+   
+   #  process = subprocess.Popen(['python', '/opt/scripts/telnet.py'],
+   #                         stdout=subprocess.PIPE,
+   #                         universal_newlines=True)
+   #  while True:
+   #      output = process.stdout.readline()
+   #      print(output.strip())
+   #      # Do something else
+   #      return_code = process.poll()
+   #      if return_code is not None:
+   #          print('OUTPUT =', return_code)
+   #          # Process has finished, read rest of the output
+   #          for output in process.stdout.readlines():
+   #              print(output.strip())
+   #          break

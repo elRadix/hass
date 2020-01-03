9fbc591d141d6745cea2c305e07033f1ac33ef69
elradix <elrachid@gmail.com>
2020-01-03 13:15:38 +0100

    AD Telnet v1

---
+++ appdaemon/apps/telnet.py
@@ -0,0 +1,41 @@
+import appdaemon.plugins.hass.hassapi as hass
+import subprocess
+import datetime
+import time
+class telnet(hass.Hass):
+
+ def initialize(self):
+    self.listen_state(self.telnet_cb, 'sensor.error')
+
+ def telnet_cb(self, entity, attribute, old, new, kwargs):
+    result = subprocess.run(['/opt/scripts/telnet.sh'], capture_output=True)
+    print(result.stdout)
+    print(result.stderr)
+    self.log(self.args)
+
+
+    # if ",0" not in failed:
+    #   while self.get_state('binary_sensor.easyplus_telnet') == 'off' and loop <=4:
+    #      loop+=1
+    #      self.call_service("notify/dageraad", message = ("Failed switch: {}".format(failed, )))
+    #      self.turn_off('switch.easyplus')
+    #      time.sleep(2)
+    #      self.turn_on('switch.easyplus')
+    #      time.sleep(35)
+    #      telnet = self.get_state('binary_sensor.easyplus_telnet')
+    #      easyplus = self.get_state('binary_sensor.easyplus_telnet')
+    #      self.log("telnet state %s", telnet)
+    #      self.log("easyplus {} and telnet {} - restart #{}".format(easyplus, telnet, loop))
+    #      self.call_service("notify/dageraad", message = ("easyplus {} and telnet {} - restart #{}".format(easyplus, telnet, loop)))
+    #   else:
+    #      if self.get_state('binary_sensor.easyplus_telnet') == 'off' and loop >3:
+    #        self.log("Easyplud failed after {} tries, telnet is {} ".format(loop, telnet))
+    #        self.call_service("notify/dageraad", message = ("Easyplus failed after {} tries, telnet still {} ".format(loop, telnet)))
+    #      if self.get_state('binary_sensor.easyplus_telnet') == 'on' and loop <=3:
+    #        returncode = subprocess.run("{} {}".format(script, failed), shell=True, capture_output=True).stdout
+    #        self.log("{} {}".format(script, failed))
+    #        self.call_service("notify/dageraad", message = ("switch turned {} succesfully".format(easyplus)))
+    #        self.set_state("sensor.error", state=".sh '0,0'")
+    #        process = subprocess.Popen(cmdtelnet, shell=True, stdout=subprocess.PIPE)
+    #        process.wait()
+    #        print (process.returncode)

23e0a64a62eb272a5e1dfa38bfeb44f571df692d
elradix <elrachid@gmail.com>
2020-01-04 18:49:55 +0100

    telnet_scan v5

--- appdaemon/apps/telnet_scan.py
+++ appdaemon/apps/telnet_scan.py
@@ -3,20 +3,17 @@ import datetime
 import telnetlib
 import time
 import re
-import threading
+import time
 
 class telnet_scan(hass.Hass):
 
-
-
  def initialize(self):
-    #self.listen_state(self.get_easyplus, 'input_boolean.night')
-    threading.Timer(10.0, self.get_easyplus).start() # called every minute
+    self.listen_state(self.motion, "switch.easyplus", new = "on")
+    self.run_in(self.get_easyplus, 60)
 
  def get_easyplus(self, entity, attribute, old, new, kwargs):
     self.log("getting easyplus log...")
     tn = telnetlib.Telnet("192.168.3.61",2024)
-#    tn.write("r\n".encode())
     tn.write("pass apex\r\n".encode())
     time.sleep(0.5)
     tn.write("getdata\r\n".encode())
@@ -24,6 +21,7 @@ class telnet_scan(hass.Hass):
     data=tn.read_very_eager()
     #sys.stdout.write(data)
     tn.close()
+    self.log("getting easyplus completed")
     self.log(data)
     if "DigitalOut 33,ON".encode() in data:
        self.log("Microwave ON")
@@ -38,7 +36,8 @@ class telnet_scan(hass.Hass):
     #self.set_state("input_text.band", state = data2 ) #BCK - need to parse data2
 
 
-
+#  def initialize(self):
+    # self.listen_state(self.get_easyplus, 'input_boolean.night')
 #   def initialize(self):
 #     #Run callback to run get_bands every 30s starting now
 #     # self.run_every(self.get_easyplus, datetime.datetime.now(),5)

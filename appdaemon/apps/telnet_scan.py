4c765984162dea3dccb8cb76d95e968c19d6a913
elradix <elrachid@gmail.com>
2020-01-04 16:53:19 +0100

    telnet scan

---
+++ appdaemon/apps/telnet_scan.py
@@ -0,0 +1,26 @@
+import appdaemon.plugins.hass.hassapi as hass
+import datetime
+import telnetlib
+import time
+import re
+
+
+class TelnetEasyplus(hass.Hass):
+
+  def initialize(self):
+    #Run callback to run get_bands every 30s starting now
+        self.run_every(self.get_easypluslog, self.datetime(), 30)
+
+  def get_easypluslog(self, kwargs):
+    #self.log("getting band...")
+    tn = telnetlib.Telnet("192.168.3.61",2024)
+    tn.write("pass apex\r\n".encode())
+    time.sleep(0.5)
+    tn.write("getdata\r\n".encode())
+    time.sleep(0.5)
+    data=tn.read_very_eager()
+    sys.stdout.write(data)
+    tn.close()
+    #data2=re.findall(r'LTE band: *(\S*)', data.decode())
+    #self.set_state("input_text.band", state = data2 ) #BCK - need to parse data2
+    self.log(data)

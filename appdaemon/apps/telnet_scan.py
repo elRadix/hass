3236cb65ccabebf16dfaf49f48b817f3217e92c9
elradix <elrachid@gmail.com>
2020-01-05 01:42:28 +0100

    Telnet Scanner

--- appdaemon/apps/telnet_scan.py
+++ appdaemon/apps/telnet_scan.py
@@ -23,12 +23,12 @@ class telnet_scan(hass.Hass):
      tn.write("pass apex\r\n".encode())
      while self.get_state('binary_sensor.easyplus_telnet') == 'on':
        data=tn.read_very_eager()
-       if ">".encode('ascii') in data:
-        cleaned =re.sub(">",'',data).decode('ascii')
-        type = cleaned.split()[0]
-        state = cleaned.split()[1]
-        self.log("Type= %s", type)
-        self.log("State= %s", state)
+       if ">".encode() in data:
+         self.log(data)
+        # type = data.split()[0]
+        # state = data.split()[1]
+        # self.log("Type %s", type)
+        # self.log("State %s", state)
      self.log(self.args)
 
 

8d0768fe66a2a020515d426369a061faaca86154
elradix <elrachid@gmail.com>
2020-01-05 01:02:38 +0100

    Telnet Scanner v23

--- appdaemon/apps/telnet_scan.py
+++ appdaemon/apps/telnet_scan.py
@@ -24,7 +24,7 @@ class telnet_scan(hass.Hass):
      while self.get_state('binary_sensor.easyplus_telnet') == 'on':
        data=tn.read_very_eager()
        if ">".encode() in data:
-        type = data.split()[1].encode()
+        type = data.split()[1]
         self.log(type)
      self.log(self.args)
 
